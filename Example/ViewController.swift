777b2cbe0f33a4a6174f466ad870ad449d5c8891
Leandro Alonso <contato@leandroalonso.com>
2020-01-29 18:58:06 -0300

    Improvements on the Example
    * Add titles
    * Change description location
    * Shows a warning if it's running in the simulator

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -2,9 +2,12 @@ import UIKit
 
 class ViewController: UITableViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        #if !targetEnvironment(simulator)
+        tableView.footerView(forSection: 2)?.isHidden = true
+        #endif
     }
 
 }
