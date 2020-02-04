a4178ed9b0f3622faafb41dd12503e26c5523a32
Leandro Alonso <contato@leandroalonso.com>
2020-02-04 13:19:29 -0300

    Load the thumbnail based on screen size
    That way the image will not appear blurried.

--- Sources/Extensions/PHAsset+AsyncImage.swift
+++ Sources/Extensions/PHAsset+AsyncImage.swift
@@ -33,7 +33,8 @@ extension PHAsset: AsyncImage {
         options.deliveryMode = .opportunistic
         options.version = .current
         options.resizeMode = .fast
-        let requestID = PHImageManager.default().requestImage(for: self, targetSize: CGSize(width: pixelWidth, height: pixelHeight), contentMode: .default, options: options) { image, info in
+        options.isNetworkAccessAllowed = true
+        let requestID = PHImageManager.default().requestImage(for: self, targetSize: UIScreen.main.bounds.size, contentMode: .default, options: options) { image, info in
             guard let image = image else {
                 finishedRetrievingThumbnail(nil)
                 return
