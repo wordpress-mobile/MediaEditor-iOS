247430de661978bd0f90d26b3c5d11c20bb3e99a
kean <grebenyuk.alexander@gmail.com>
2024-06-28 08:27:42 -0400

    Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.10
+
+import Foundation
+import PackageDescription
+
+let package = Package(
+    name: "MediaEditor",
+    platforms: [.iOS(.v15)],
+    products: [
+        .library(name: "MediaEditor", targets: ["MediaEditor"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/TimOliver/TOCropViewController", from: "2.5.3")
+    ],
+    targets: [
+        .target(
+            name: "MediaEditor", 
+            dependencies: [
+                .product(name: "CropViewController", package: "TOCropViewController")
+            ])
+    ]
+)
